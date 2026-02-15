2ba400249c07b2a6c6a4f0320c14bca633c04af6
Olena Zozulia <lenazozula05@gmail.com>
2026-02-15 12:36:29 +0200

    Add roles

---
+++ childspace-backend/Utility/StaticDetail.cs
@@ -0,0 +1,10 @@
+﻿namespace childspace_backend.Utility
+{
+    public class StaticDetail
+    {
+        public const string Role_SuperAdmin = "SuperAdmin";
+        public const string Role_CenterAdmin = "CenterAdmin";
+        public const string Role_Teacher = "Teacher";
+        public const string Role_Parent = "Parent";
+    }
+}
