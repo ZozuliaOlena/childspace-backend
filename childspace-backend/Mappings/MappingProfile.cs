4c5126bc839b365b73f9a0317b097e65f6d34b79
Olena Zozulia <lenazozula05@gmail.com>
2026-02-15 12:36:29 +0200

    Add auto mapping

---
+++ childspace-backend/Mappings/MappingProfile.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using childspace_backend.Models;
+using childspace_backend.Models.DTOs;
+using childspace_backend.Models.Enums;
+
+namespace childspace_backend.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserDto>();
+
+            CreateMap<Child, ChildDto>()
+                .ForMember(dest => dest.Groups, opt => opt.MapFrom(src => src.GroupChildren))
+                .ForMember(dest => dest.Attendances, opt => opt.MapFrom(src => src.Attendances));
+
+            CreateMap<GroupChild, GroupChildDto>();
+
+            CreateMap<Center, CenterDto>();
+ 
+            CreateMap<Schedule, ScheduleDto>();
+
+            CreateMap<Attendance, AttendanceDto>()
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()));
+
+            CreateMap<Material, MaterialDto>();
+
+            CreateMap<Message, MessageDto>();
+
+            CreateMap<Chat, ChatDto>();
+
+            CreateMap<TrialRequest, TrialRequestDto>();
+
+            CreateMap<Subject, SubjectDto>();
+
+            CreateMap<UserChat, UserChatDto>();
+        }
+    }
+}
