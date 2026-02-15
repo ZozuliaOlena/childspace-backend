97dc937c834660cf3d890f3a2a0f586b664b26fa
Olena Zozulia <lenazozula05@gmail.com>
2026-02-15 12:36:28 +0200

    Add project files.

---
+++ childspace-backend/Program.cs
@@ -0,0 +1,29 @@
+namespace childspace_backend
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddAuthorization();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.Run();
+        }
+    }
+}
