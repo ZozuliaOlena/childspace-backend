3e317c0540114cf9f72282a614254c39421ea7f4
Olena Zozulia <lenazozula05@gmail.com>
2026-06-01 17:25:44 +0300

    Add error message in login dto

--- childspace-backend/Models/DTOs/LoginDto.cs
+++ childspace-backend/Models/DTOs/LoginDto.cs
@@ -1,8 +1,14 @@
-﻿namespace childspace_backend.Models.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace childspace_backend.Models.DTOs
 {
     public class LoginDto
     {
+        [Required(ErrorMessage = "Поле Email не може бути порожнім.")]
+        [EmailAddress(ErrorMessage = "Невірний формат Email.")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Поле Пароль не може бути порожнім.")]
         public string Password { get; set; }
     }
 }
